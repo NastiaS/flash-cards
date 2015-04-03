676a1bfb307dda96fb0ad6f79ba332dd71db36a2
NastiaS <anastasiasergienko@gmail.com>
2015-04-03 15:47:51 -0400

    categories done

--- public/app.js
+++ public/app.js
@@ -1,28 +1,54 @@
-var app = angular.module('flashCards', []);
+var app = angular.module('FlashCards', []);
 
-app.value('whateverName', [
-    {
-        question: 'C. What is Angular?',
-        answers: [
-            { text: 'A front-end framework for great power!', correct: true },
-            { text: 'Something lame, who cares, whatever.', correct: false },
-            { text: 'Some kind of fish, right?', correct: false }
-        ]
-    },
-    {
-        question: 'A. What is a controller?',
-        answers: [
-            { text: 'Something that manages my front-end routes', correct: false },
-            { text: 'A function that allows me to manage a scope', correct: true },
-            { text: 'An Angular template', correct: false }
-        ]
-    },
-    {
-        question: 'B. What does {{ }} do?',
-        answers: [
-            { text: 'It runs some Javascript', correct: false },
-            { text: 'It looks for variables in HTML', correct: false },
-            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
-        ]
+app.controller('FlashCardController', function($scope){
+//console.log(FlashCardsFactory.getFlashCards());
+
+
+$scope.answerQuestion = function(answer){
+    $scope.answered=true;
+    $scope.answeredCorrectly=answer;
+}
+
+
+});
+
+app.factory("FlashCardsFactory", function($http){
+     
+   
+    return {
+        getFlashCards: function(category){
+            var req = {
+                method: 'GET',
+                url: '/cards',
+                params: { category: category }
+                }
+
+           return $http(req).then(function(response){
+                return response.data;
+            });
+        }};
+});
+
+app.controller("MainController", function($scope, FlashCardsFactory){
+    FlashCardsFactory.getFlashCards().then(function(FlashCards){
+        //console.log("Got here")
+        $scope.flashCards= FlashCards;
+    });
+    $scope.categories = [
+        'MongoDB',
+        'Express',
+        'Angular',
+        'Node'
+    ];
+    $scope.getCategoryCards= function(category){
+        //console.log(FlashCardsFactory.getFlashCards(category));
+        FlashCardsFactory.getFlashCards(category).then(function(flashCards){
+            $scope.flashCards = flashCards;
+            $scope.currentCategory = category;
+            // console.log(category);
+            // console.log($scope.currentCategory)
+        });
+
     }
-]);
+
+});
